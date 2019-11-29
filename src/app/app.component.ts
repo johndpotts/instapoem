4efe1e0fce318a3460999d251d1a8fc37d0258be
John Potts <jdpotts189@gmail.com>
2019-11-29 07:21:35 -0500

    transitions on limerick section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,32 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { query, style, group, animate } from '@angular/animations';
+import { ActivatedRoute } from '@angular/router';
+
+const left = [
+  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
+  group([
+      query(':enter', [style({ transform: 'translateX(-100%)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
+          optional: true,
+      }),
+      query(':leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(100%)' }))], {
+          optional: true,
+      }),
+  ]),
+];
+
+const right = [
+  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
+  group([
+      query(':enter', [style({ transform: 'translateX(100%)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
+          optional: true,
+      }),
+      query(':leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(-100%)' }))], {
+          optional: true,
+      }),
+  ]),
+];
 
 @Component({
   selector: 'app-root',
@@ -10,38 +36,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  public appPages = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
 
-    {
-      title: 'Limerick',
-      url: '/limerick',
-      icon: 'flame'
-    },
-    {
-      title: 'Haiku',
-      url: '/haiku',
-      icon: 'infinite'
-    },
-    {
-      title: 'Settings',
-      url: '/settings',
-      icon: 'settings'
-    }
-  ];
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private route: ActivatedRoute
   ) {
     this.initializeApp();
   }
+  animationState: number;
 
+  onActivate($event) {
+    this.animationState = this.route.firstChild.snapshot.data['routeIdx'];
+  }
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
--- src/app/limerick/limerick.page.ts
+++ src/app/limerick/limerick.page.ts
@@ -2,37 +2,47 @@ import { Component, OnInit } from '@angular/core';
 import { Pronoun } from '../services/pronoun.service';
 import { LimerickService } from '../services/limerick.service';
 import { Limerick } from '../models/limerick.model';
+import { Animations } from '../animations';
+const slideinout = Animations.slideinout
 
 @Component({
   selector: 'app-limerick',
   templateUrl: './limerick.page.html',
   styleUrls: ['./limerick.page.scss'],
+  animations:[slideinout]
 })
 export class LimerickPage implements OnInit {
-name: String;
-gender: Pronoun;
-limerick: Limerick;
+  name: String;
+  gender: Pronoun;
+  limerick: Limerick;
+  isChangingInfo: Boolean = false;
 
   constructor(private limerickService: LimerickService) { }
 
   ngOnInit() {
   }
 
-  public handleNameValue(e){
-  this.name = e.target.value;
-}
-public handleGenderValue(e){
-  this.gender = e.target.value;
-}
-  hasRequiredInfo(){
+  public handleNameValue(e) {
+    this.name = e.target.value;
+  }
+  public handleGenderValue(e) {
+    this.gender = e.target.value;
+  }
+  hasRequiredInfo() {
     return this.name && this.name.length &&
-   this.gender && this.gender.length;
+      this.gender && this.gender.length;
 
   }
 
-  generateLimerick(){
-this.limerick = this.limerickService.getLimerick(this.name, this.gender);
+  generateLimerick() {
+    this.isChangingInfo = false;
+    this.limerick = this.limerickService.getLimerick(this.name, this.gender);
   }
-
-
+  changeInfo() {
+    this.isChangingInfo = true;
+    this.limerick = undefined;
+  }
+  getExpanded(){
+    return this.limerick ? null : 'block';
+  }
 }
